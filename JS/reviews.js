2e57322af0e7221499b3c81ed58a3415f047b160
Zvezegor <ferocaritas@gmail.com>
2022-07-12 23:54:07 +0300

    starting swiper

--- JS/reviews.js
+++ JS/reviews.js
@@ -1,8 +1,4 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
@@ -12,10 +8,5 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
   },
 });
